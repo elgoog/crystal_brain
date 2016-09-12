a7e9da872c24c1a9b31ef522cf07094aee49e8a5
elgoog <elgoog.elibom@gmail.com>
2016-09-12 19:29:51 +0900

    Copy codes from RubyBrain and change the extension to ".cr" from ".rb"

--- src/crystal_brain.cr
+++ src/crystal_brain.cr
@@ -1,5 +1,18 @@
-require "./crystal_brain/*"
+require "ruby_brain/version"
+require 'yaml'
+require 'pp'
 
-module CrystalBrain
-  # TODO Put your code here
+module RubyBrain
+  require "ruby_brain/nodes"
+  require "ruby_brain/layer"
+  require "ruby_brain/weights"
+  require "ruby_brain/network"
+  require "ruby_brain/trainer"
+  require 'ruby_brain/exception'
+  require 'ruby_brain/training_data_manipulator'
+  
+  module Nodes end
+  module Trainer end
+  module Exception end
+  module DataSet end
 end
